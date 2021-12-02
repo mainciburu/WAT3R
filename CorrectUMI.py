88687cf612612e9b2573fd794b5ca40ef04ab361
mainciburu <mainciburu@alumni.unav.es>
2021-12-02 17:01:51 -0500

    Update scripts

---
+++ CorrectUMI.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+
+import sys
+import os
+from optparse import OptionParser
+
+
+###### OPTIONS ######
+# options allow for arguments imported from the command line
+# they are shown from command line with script.py --help
+opts = OptionParser()
+usage = "usage: %prog [options] [inputs] Software to correct UMI reads using UMItools clusterer"
+
+opts.add_option("-b", "--MyBarcodes", help="<Read1> Accepts list of barcodes + UMI in txt")
+
+#opts.add_option("-n", "--nreads", default = 1000000, help="Number of reads to process in a given chunk")
+#opts.add_option("-r", "--ncores", default = 4, help="Number of cores for parallel processing.")
+
+opts.add_option("-o", "--output", help="Output file prefix")
+
+options, arguments = opts.parse_args()
+
+# return usage information if no argvs given
+if len(sys.argv)==1:
+	os.system(sys.argv[0]+" --help")
+	sys.exit()
+
+
+###### IMPORT MODULES 2 ######
+import pandas as pd
+from umi_tools import UMIClusterer
+
+
+if __name__ == "__main__":
+
+	###### INPUT ######
+	bf = options.MyBarcodes
+
+	outname = options.output
+	#cpu = int(options.ncores)
+	#n = int(options.nreads)
+    ##############
+
+	df = pd.read_csv(bf, index_col = None, header = None, names = ['BC_UMI'])
+	df['UMI'] = [x[16:] for x in df.BC_UMI]         # extract UMIs
+	df['BYTEUMI'] = [x.encode() for x in df.UMI]    # Encode
+	total = len(df.UMI) - sum(df.UMI=="N"*12)       # Count valid UMI reads
+	UMIcount = df.BYTEUMI.value_counts()            # Count reads for each UMI
+	mydict = UMIcount.to_dict()					    
+
+	## Cluster UMIs
+	clusterer = UMIClusterer(cluster_method="directional")
+	clustered_umis = clusterer(mydict, threshold=1)
+
+	## Dictionary with unique UMIs as key and corrected equivalent as value
+	mydict = {}
+	for x in clustered_umis:
+		correct = x[0]          # correct (more frequent) UMI
+		if (len(x) > 1):
+			for each in x[1:]:
+				mydict[each] = correct
+		mydict[correct] = correct
+	
+	
+	df['CORRECTED_UMI'] = [mydict[x] for x in df.BYTEUMI]
+	df['CORRECTED_UMI'] = [x.decode() for x in df.CORRECTED_UMI]
+	corrected = sum(df.UMI != df.CORRECTED_UMI)
+	df.CORRECTED_UMI.to_csv(outname + '.txt', index = False, index_label = None, header = None)
+
+	print("Completed -------------------------------------- \n")
+	print("Total UMI reads: " + str(total) + "\n")
+	print("Corrected UMI reads: " + str(corrected) + "\n")
+	print("% Corrected UMI reads: " + str(round((corrected/total)*100, 2)) + "%\n")
+	
+
+
+
+
+
+
+
---
+++ QCplots_preFiltering.r
@@ -0,0 +1,57 @@
+#!/usr/bin/Rscript
+
+################################
+## QC Plots - qscore filtering
+################################
+
+args=(commandArgs(TRUE))
+print(args)
+BaseFolder<-args[1]
+
+library(ggplot2)
+library(dplyr)
+
+setwd(paste0(BaseFolder, "/warpt"))
+
+ns<-try(read.table("./QC/ns.txt"))
+if(class(ns)=="try-error"){
+  ns<-NULL
+}
+
+qscore<-read.table("./QC/qscore.txt")
+
+if(!is.null(ns)){
+  pdf("./QC/QCplots_preFiltering.pdf", width = 15, height = 10, useDingbats = F)
+
+  # Reads with >0 Ns => N number vs average Q score
+  plotter<-data.frame(Ns=ns$V1, Qscore=qscore$V2[ns$V2])
+  n<-nrow(plotter)
+  p1<-ggplot(plotter, aes(Ns, Qscore)) + geom_point(size = 0.1) + theme_bw() +
+    geom_hline(yintercept = 25, color = "red") + ggtitle(paste0(n, " Reads with Ns")) + theme(text=element_text(size=16))
+
+  # Reads with no Ns => average Q score distribution
+  plotter<-data.frame(Qscore=qscore$V2[-(ns$V2)])
+  xmax<-round(max(plotter$Qscore))+1
+  xmin<-round(min(plotter$Qscore))-1
+  n<-nrow(plotter)
+  p2<-ggplot(plotter, aes(Qscore)) + geom_histogram(binwidth = 1, color="black") + 
+    scale_x_reverse(breaks=xmax:xmin) + theme_bw() + geom_vline(xintercept = 25, color = "red") + 
+    ggtitle(paste0(n, " Reads with no Ns")) + theme(text=element_text(size=16))
+  print(p1)
+  print(p2)
+  dev.off()
+}
+
+if(is.null(ns)){
+  pdf("./QC/QCplots_preFiltering.pdf", width = 15, height = 10, useDingbats = F)
+  # Reads with no Ns => average Q score distribution
+  plotter<-data.frame(Qscore=qscore$V2)
+  xmax<-round(max(plotter$Qscore))+1
+  xmin<-round(min(plotter$Qscore))-1
+  n<-nrow(plotter)
+  p1<-ggplot(plotter, aes(Qscore)) + geom_histogram(binwidth = 1, color="black") + 
+    scale_x_reverse(breaks=xmax:xmin) + theme_bw() + geom_vline(xintercept = 25, color = "red") + 
+    ggtitle(paste0(n, " Reads with no Ns")) + theme(text=element_text(size=16))
+  print(p1)
+  dev.off()
+}
