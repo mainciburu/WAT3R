72c267593d3443b1dc178201084586dcaa5a4578
mainciburu <mainciburu@alumni.unav.es>
2021-12-23 16:20:18 -0500

    update

--- scripts/QCplots_clustering.r
+++ scripts/QCplots_clustering.r
@@ -40,12 +40,14 @@ plotter<-data.frame(BC.UMI=x$BC.UMI[i],
                     Ratio=ratio)
 plotter$logRatio<-log2(plotter$Ratio+0.05)
 plotter$logFrequency.BC.UMI<-log2(plotter$Frequency.BC.UMI)
+nread<-sum(plotter$Frequency.BC.UMI.Cluster[plotter$Proportion>0.5&plotter$Ratio>2])
+pct.read<-round((nread/sum(plotter$Frequency.BC.UMI))*100, 2)
 
 pdf("./wat3r/QC/QCplot_clusters.pdf", width = 15, height = 10)
 ggplot(plotter, aes(Proportion, logRatio, color=logFrequency.BC.UMI)) + geom_point(size = 0.05) + 
   theme_bw() + scale_color_gradientn(colours = rev(brewer.pal(11, "Spectral"))) + 
   geom_vline(xintercept = 0.5, colour="red") + geom_hline(yintercept = 1, colour="red") + ylim(c(0, round(max(plotter$logRatio))+1)) + 
-  theme(text=element_text(size=20))
+  theme(text=element_text(size=20)) + ggtitle("TCR Sequences Clustering", subtitle=paste0(nread, " (", pct.read, "%)", " reads passing default filters (Proportion > 0.5, Ratio > 2)"))
 dev.off()
 
 write.table(plotter, file = "./wat3r/QC/BC_UMI_cluster_metrics.txt", quote = F)
